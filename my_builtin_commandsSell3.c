f4fd2578e3c7144e0b58843b7a7b553a42a6da98
EL HAKIK Amina <amina@gmail.com>
2023-09-08 09:30:52 -0700

    task 3 : ALX

---
+++ my_builtin_commandsSell3.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _myhistoryShll - this function will : displays the history list, one command by line, preceded
+ * with line numbers, starting at 0.
+ * @my_infoShll: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always returns 0
+ */
+
+int _myhistoryShll(my_info_stShll *my_infoShll)
+{
+	my_print_listShll(my_infoShll->my_historyShll);
+	return (0);
+}
+
+/**
+ * my_unset_aliasShll - this function will : sets alias to string
+ * @my_infoShll: parameter struct
+ * @my_strShll: the string alias
+ * Return: Always returns 0 on success, 1 on error
+ */
+
+int my_unset_aliasShll(info_st *info, char *str)
+{
+	char *my_pShll, my_cShll;
+	int my_retShll;
+
+	my_pShll = _strchrShll(my_strShll, '=');
+	if (!my_pShll)
+		return (1);
+	my_cShll = *my_pShll;
+	*my_pShll = 0;
+	my_retShll = my_delete_node_at_indexShll(&(my_infoShll->my_aliasShll),
+		my_get_node_indexShll(my_info->my_aliasShll, my_node_starts_withShll(my_infoShll->my_aliasShll, my_strShll, -1)));
+	*my_pShll = my_cShll;
+	return (my_retShll);
+}
+
